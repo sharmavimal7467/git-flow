ad9ab296ccb969455ba6d7e27fdfe2f17b7b0cad
sharmavimal7467 <sharmavimal7467@gmail.com>
2025-07-18 13:40:30 +0530

    feat:content change in pages file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,7 @@ export default function Home() {
           height={38}
           priority
         />
+        <h1>Check for git flow merge</h1>
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
